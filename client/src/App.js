70bc34aea6b122857b9acfb528813cc8fe43662c
Sergeygeek <sukhorukov1986@yandex.ru>
2020-07-15 08:02:49 +0400

    Login User

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
-import  Register  from './components/auth/Register';
-import { Login } from './components/auth/Login';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Alert from './components/layout/Alert'
 //Redux
 import { Provider } from 'react-redux';
